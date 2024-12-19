85d0a45013d4c1a8aae1a3d9c94cd2d4c5b42540
美木 <miki211287@gmail.com>
2024-12-19 15:56:59 +0700

    add today submission

---
+++ src/rustfinity_com/advent_2024/day19.rs
@@ -0,0 +1,32 @@
+// https://www.rustfinity.com/practice/rust/challenges/aor-2024-19/description
+
+use std::marker::PhantomData;
+
+pub struct Empty;
+pub struct Ready;
+pub struct Flying;
+
+pub struct Sleigh<State> {
+  state: PhantomData<State>
+}
+impl Sleigh<Empty> {
+  pub fn new() -> Self {
+    Self { state: PhantomData }
+  }
+  pub fn load(self) -> Sleigh<Ready> {
+    Sleigh { state: PhantomData }
+  }
+}
+impl Sleigh<Ready> {
+  pub fn take_off(self) -> Sleigh<Flying> {
+    Sleigh { state: PhantomData }
+  }
+  pub fn unload(self) -> Sleigh<Ready> {
+    Sleigh { state: PhantomData }
+  }
+}
+impl Sleigh<Flying> {
+  pub fn land(self) -> Sleigh<Ready> {
+    Sleigh { state: PhantomData }
+  }
+}
